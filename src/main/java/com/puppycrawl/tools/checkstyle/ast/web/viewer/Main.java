0bbaf15d98ce66af41657f46951e99dc8c694616
dyaroslavtsev <Daniil.Yaroslavtsev@gmail.com>
2016-04-28 23:06:03 +0000

    Allow uploading files with Java code

--- src/main/java/com/puppycrawl/tools/checkstyle/ast/web/viewer/Main.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/ast/web/viewer/Main.java
@@ -1,5 +1,7 @@
 package com.puppycrawl.tools.checkstyle.ast.web.viewer;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
 import java.io.StringReader;
 import java.util.Comparator;
 import java.util.Set;
@@ -33,6 +35,9 @@ import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.TextArea;
 import com.vaadin.ui.TreeTable;
+import com.vaadin.ui.Upload;
+import com.vaadin.ui.Upload.FinishedEvent;
+import com.vaadin.ui.Upload.SucceededEvent;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 
@@ -49,7 +54,7 @@ public class Main extends Application {
 
         WebAppContext servletContextHandler = new WebAppContext();
         servletContextHandler.setContextPath("/");
-       servletContextHandler.setResourceBase("src/main/resources/webapp");
+        servletContextHandler.setResourceBase("src/main/resources/webapp");
         ClassList clist = ClassList.setServerDefault(server);
         clist.addBefore(JettyWebXmlConfiguration.class.getName(), AnnotationConfiguration.class.getName());
         servletContextHandler.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*(/target/classes/|.*.jar)");
@@ -104,9 +109,21 @@ public class Main extends Application {
         buttonLayout.setSpacing(true);
         buttonLayout.setMargin(true, false, true, false);
 
+        Button loadSourceButton = new Button("Load source ...");
+        loadSourceButton.setImmediate(true);
+        loadSourceButton.addListener(new LoadSourceListener());
+        buttonLayout.addComponent(loadSourceButton);
+
         Button displayTreeButton = new Button("Display Tree");
         displayTreeButton.setImmediate(true);
-        displayTreeButton.addListener(new ParsingClickListener());
+        displayTreeButton.addListener(new ClickListener() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void buttonClick(ClickEvent event) {
+                displayAst();
+            }
+        });
         buttonLayout.addComponent(displayTreeButton);
 
         Button expandByOneLevelButton = new Button("Expand by one level");
@@ -173,160 +190,184 @@ public class Main extends Application {
         setMainWindow(window);
     }
 
-    private final class ParsingClickListener implements Button.ClickListener {
-        private static final long serialVersionUID = 1L;
+    private void displayErrorDialog(Exception e, String dialogTitle, String messageTitle, String errorText) {
+        final Window dialog = new Window(dialogTitle);
+        dialog.setModal(true);
+        dialog.setWidth("500px");
+        dialog.setHeight("350px");
+        window.addWindow(dialog);
 
-        public void buttonClick(ClickEvent event) {
-            String fullText = codeArea.getValue().toString();
+        VerticalLayout layout = new VerticalLayout();
+        layout.setSizeFull();
 
-            DetailAST astTree = null;
-            try {
-                astTree = parse(fullText);
-                fillWithAstTree(tree, astTree);
-            } catch (RecognitionException | TokenStreamException e) {
-                displayErrorDialog(e, "Error", "Parsing Error", e.toString());
+        TextArea errorTextArea = new TextArea(messageTitle);
+        errorTextArea.setSizeFull();
+        errorTextArea.setValue(errorText);
+        layout.addComponent(errorTextArea);
+
+        Button gotItButton = new Button("Got it", new Button.ClickListener() {
+            private static final long serialVersionUID = 1L;
+
+            public void buttonClick(ClickEvent event) {
+                window.removeWindow(dialog);
+            }
+        });
+
+        layout.addComponent(gotItButton);
+        layout.setExpandRatio(gotItButton, 1);
+        layout.setExpandRatio(errorTextArea, 100);
+
+        dialog.setContent(layout);
+    }
+
+    private void displayAst() {
+        String fullText = codeArea.getValue().toString();
+
+        DetailAST astTree = null;
+        try {
+            astTree = parse(fullText);
+            fillWithAstTree(tree, astTree);
+        } catch (RecognitionException | TokenStreamException e) {
+            displayErrorDialog(e, "Error", "Parsing Error", e.toString());
+        }
+    }
+
+    private static DetailAST parse(String fullText) throws RecognitionException, TokenStreamException {
+
+        GeneratedJavaLexer lexer = new GeneratedJavaLexer(new StringReader(fullText));
+        lexer.setFilename("test");
+
+        // Ugly hack to skip comments support for now
+        lexer.setCommentListener(new CommentListener() {
+            @Override
+            public void reportSingleLineComment(String aType, int aStartLineNo, int aStartColNo) {
+            }
+
+            @Override
+            public void reportBlockComment(String type, int startLineNo, int startColNo, int endLineNo, int endColNo) {
+            }
+        });
+        lexer.setTreatAssertAsKeyword(true);
+        lexer.setTreatEnumAsKeyword(true);
+
+        final GeneratedJavaRecognizer parser = new GeneratedJavaRecognizer(lexer);
+        parser.setFilename("file");
+        parser.setASTNodeClass(DetailAST.class.getName());
+        parser.compilationUnit();
+
+        return (DetailAST) parser.getAST();
+    }
+
+    private static void fillWithAstTree(AstTreeTable tree, DetailAST astTree) {
+
+        tree.removeAllItems();
+
+        for (DetailAST token : collectTokensInHumanReadOrder(astTree)) {
+
+            addItem(tree, token);
+
+            DetailAST parent = token.getParent();
+            if (parent != null) {
+                addIfNotExist(tree, parent);
+                tree.setParent(token, parent);
             }
         }
 
-        private void displayErrorDialog(Exception e, String dialogTitle, String messageTitle, String errorText) {
-            final Window dialog = new Window(dialogTitle);
+        tree.makeLeafItemsNonExpandable();
+    }
+
+    private static Set<DetailAST> collectTokensInHumanReadOrder(DetailAST astTree) {
+
+        Set<DetailAST> allTokens = Sets.newTreeSet(new Comparator<DetailAST>() {
+            @Override
+            public int compare(DetailAST o1, DetailAST o2) {
+                int result = Integer.compare(o1.getLineNo(), o2.getLineNo());
+                if (result == 0) {
+                    result = Integer.compare(o1.getColumnNo(), o2.getColumnNo());
+                }
+                if (result == 0) {
+                    result = o1.toString().compareTo(o2.toString());
+                }
+                return result;
+            }
+        });
+
+        // Iterate over the tree and add all tokens to the plain set
+
+        DetailAST curNode = astTree;
+        while (curNode != null) {
+
+            allTokens.add(curNode);
+
+            DetailAST toVisit = curNode.getFirstChild();
+            while ((curNode != null) && (toVisit == null)) {
+                toVisit = curNode.getNextSibling();
+                if (toVisit == null) {
+                    curNode = curNode.getParent();
+                }
+            }
+            curNode = toVisit;
+        }
+
+        return allTokens;
+    }
+
+    private static void addIfNotExist(TreeTable tree, DetailAST node) {
+        if (!tree.containsId(node)) {
+            addItem(tree, node);
+        }
+    }
+
+    private static void addItem(TreeTable tree, DetailAST node) {
+        Object[] cells = new Object[] { node.toString(), TokenTypes.getTokenName(node.getType()), node.getLineNo(),
+                node.getColumnNo(), node.getText() };
+        tree.addItem(cells, node);
+    }
+
+    private class LoadSourceListener implements ClickListener {
+
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public void buttonClick(ClickEvent event) {
+            final Window dialog = new Window("Load source");
             dialog.setModal(true);
-            dialog.setWidth("500px");
-            dialog.setHeight("350px");
+            dialog.setWidth("600px");
+            dialog.setHeight("400px");
             window.addWindow(dialog);
 
             VerticalLayout layout = new VerticalLayout();
             layout.setSizeFull();
 
-            TextArea errorTextArea = new TextArea(messageTitle);
-            errorTextArea.setSizeFull();
-            errorTextArea.setValue(errorText);
-            layout.addComponent(errorTextArea);
-
-            Button gotItButton = new Button("Got it", new Button.ClickListener() {
+            final ByteArrayOutputStream fos = new ByteArrayOutputStream();
+            
+            Upload upload = new Upload("Upload file ...", new Upload.Receiver() {
                 private static final long serialVersionUID = 1L;
 
-                public void buttonClick(ClickEvent event) {
+                @Override
+                public OutputStream receiveUpload(String filename, String mimeType) {
+                    return fos; // Return the output stream to write to
+                }
+            });
+
+            upload.addListener(new Upload.FinishedListener() {
+                private static final long serialVersionUID = 1L;
+
+                @Override
+                public void uploadFinished(FinishedEvent event) {
+                    String code = fos.toString();
+                    codeArea.setValue(code);
+                    displayAst();
                     window.removeWindow(dialog);
                 }
             });
+            upload.setButtonCaption("Done");
+            layout.addComponent(upload);
 
-            layout.addComponent(gotItButton);
-            layout.setExpandRatio(gotItButton, 1);
-            layout.setExpandRatio(errorTextArea, 100);
+            layout.setExpandRatio(upload, 1);
 
             dialog.setContent(layout);
         }
-
-        private DetailAST parse(String fullText) throws RecognitionException, TokenStreamException {
-
-            GeneratedJavaLexer lexer = new GeneratedJavaLexer(new StringReader(fullText));
-            lexer.setFilename("test");
-
-            // Ugly hack to skip comments support for now
-            lexer.setCommentListener(new CommentListener() {
-                @Override
-                public void reportSingleLineComment(String aType, int aStartLineNo, int aStartColNo) {
-                }
-
-                @Override
-                public void reportBlockComment(String type, int startLineNo, int startColNo, int endLineNo, int endColNo) {
-                }
-            });
-            lexer.setTreatAssertAsKeyword(true);
-            lexer.setTreatEnumAsKeyword(true);
-
-            final GeneratedJavaRecognizer parser = new GeneratedJavaRecognizer(lexer);
-            parser.setFilename("file");
-            parser.setASTNodeClass(DetailAST.class.getName());
-            parser.compilationUnit();
-
-            return (DetailAST) parser.getAST();
-        }
-
-        private void fillWithAstTree(AstTreeTable tree, DetailAST astTree) {
-
-            tree.removeAllItems();
-
-            for (DetailAST token : collectTokensInHumanReadOrder(astTree)) {
-
-                addItem(tree, token);
-
-                DetailAST parent = token.getParent();
-                if (parent != null) {
-                    addIfNotExist(tree, parent);
-                    tree.setParent(token, parent);
-                }
-            }
-
-            tree.makeLeafItemsNonExpandable();
-        }
-
-        private Set<DetailAST> collectTokensInHumanReadOrder(DetailAST astTree) {
-
-            Set<DetailAST> allTokens = Sets.newTreeSet(new Comparator<DetailAST>() {
-                @Override
-                public int compare(DetailAST o1, DetailAST o2) {
-                    int result = Integer.compare(o1.getLineNo(), o2.getLineNo());
-                    if (result == 0) {
-                        result = Integer.compare(o1.getColumnNo(), o2.getColumnNo());
-                    }
-                    if (result == 0) {
-                        result = o1.toString().compareTo(o2.toString());
-                    }
-                    return result;
-                }
-            });
-
-            // Iterate over the tree and add all tokens to the plain set
-
-            DetailAST curNode = astTree;
-            while (curNode != null) {
-
-                allTokens.add(curNode);
-
-                DetailAST toVisit = curNode.getFirstChild();
-                while ((curNode != null) && (toVisit == null)) {
-                    toVisit = curNode.getNextSibling();
-                    if (toVisit == null) {
-                        curNode = curNode.getParent();
-                    }
-                }
-                curNode = toVisit;
-            }
-
-            return allTokens;
-        }
-
-        private void addIfNotExist(TreeTable tree, DetailAST node) {
-            if (!tree.containsId(node)) {
-                addItem(tree, node);
-            }
-        }
-
-        private void addItem(TreeTable tree, DetailAST node) {
-            Object[] cells = new Object[] { node.toString(), TokenTypes.getTokenName(node.getType()), node.getLineNo(),
-                    node.getColumnNo(), node.getText() };
-            tree.addItem(cells, node);
-        }
-
-//        private String astTreeToString(DetailAST astTree) {
-//            StringBuilder sb = new StringBuilder();
-//            DetailAST curNode = astTree;
-//            while (curNode != null) {
-//                sb.append(curNode.toString());
-//                sb.append("\n");
-//                DetailAST toVisit = curNode.getFirstChild();
-//                while ((curNode != null) && (toVisit == null)) {
-//                    toVisit = curNode.getNextSibling();
-//                    if (toVisit == null) {
-//                        curNode = curNode.getParent();
-//                    }
-//                }
-//                curNode = toVisit;
-//            }
-//            return sb.toString();
-//        }
-
     }
+
 }
